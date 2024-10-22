efda655ed8df948dc2e336af6e4bdef9a02ad694
David Jumeau <djumeau@gmail.com>
2024-10-21 21:33:55 -0400

    Added item deletion.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearButton = document.getElementById("clear");
 
 function createIcon(classes) {
     const icon = document.createElement('i');
@@ -42,6 +43,19 @@ function addItem(e) {
     itemInput.value = "";
 }
 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        e.target.parentElement.parentElement.remove();
+    }    
+}
+
+function removeAllItems(e) {
+    while(itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
-
+itemList.addEventListener("click", removeItem)
+clearButton.addEventListener("click", removeAllItems);
